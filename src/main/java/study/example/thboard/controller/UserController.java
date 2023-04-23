4679eed90a796605f257fb608632d440fe8561c9
devlsy86 <infightersy@naver.com>
2023-04-23 18:14:37 +0900

    음.....

--- src/main/java/study/example/thboard/controller/UserController.java
+++ src/main/java/study/example/thboard/controller/UserController.java
@@ -1,6 +1,5 @@
 package study.example.thboard.controller;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,17 +7,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequiredArgsConstructor
 @Slf4j
-@RequestMapping(value = "/main")
-public class MainController {
+@RequestMapping(value = "/users")
+public class UserController {
 
     @GetMapping(value = "")
-    public ModelAndView main() {
+    public Object list() {
         ModelAndView mv = new ModelAndView();
-        mv.setViewName("pages/main");
+        mv.setViewName("pages/user");
         return mv;
     }
-
-
 }
