ddcf97ef76500e86e2e26540336c835d1ea373a7
devlsy86 <infightersy@naver.com>
2023-05-04 04:18:31 +0900

    보드 추가

---
+++ src/main/java/study/example/thboard/controller/BoardController.java
@@ -0,0 +1,35 @@
+package study.example.thboard.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import study.example.thboard.service.BoardService;
+import study.example.thboard.vo.BoardVo;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
+@RequestMapping(value = "board/*")
+public class BoardController {
+
+    private final BoardService boardService;
+
+    /**
+     * 게시글 목록 조회
+     * @return
+     */
+    @GetMapping("list")
+    public ModelAndView list() {
+        ModelAndView mv = new ModelAndView("pages/board");
+
+        List<BoardVo> boardList = boardService.getBoards();
+
+        mv.addObject("list", boardList);
+        return mv;
+    }
+}
